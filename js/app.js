f89a31641a7cf9767b060249fa1eee3ba3f1d602
meacoding <16675876+meacoding@users.noreply.github.com>
2018-11-05 16:56:03 -0500

    More JS scripting and style tweaks

--- js/app.js
+++ js/app.js
@@ -11,34 +11,59 @@ document.addEventListener("DOMContentLoaded", e => {
   });
 
   const phrases = [
-    "Cat got your tongue",
-    "What goes around comes around",
-    "What did Yoshi say to the programmer Enum",
-    "Link open your eyes",
-    "My toes are pristine"
+    "Cat got your tongue?",
+    "what goes around comes around.",
+    "What did Yoshi say to the programmer? Enum!",
+    "Link, open your eyes.",
+    "My toes are pristine!"
   ];
 
-  console.log(phrases[1].split("", phrases[1].length));
-
   function getRandomPhraseAsArray(arr) {
-    let random = Math.random() * (arr.length - 0) + 0;
-    let returnedArray = arr[random];
-    returnedArray = returnedArray.split("", 20);
-    console.log(returnedArray);
+    let random = Math.floor(Math.random() * (arr.length - 0)) + 0;
+    let returnedArray = arr[random].split("", arr[random].length);
     return returnedArray;
   }
   const phraseArray = getRandomPhraseAsArray(phrases);
 
+  function isLetter (char){
+    let character = char.textContent;
+    if (character === ' ') {
+      char.className = 'space';
+    } else if (character === '.' || character === ',' || character === '!' || character === '?') {
+      char.className = 'punctuation';
+    } else {
+      char.className = 'letter';
+    }
+  }
+
   function addPhraseToDisplay(arr) {
-    // do stuff any arr that is passed in, and add to `#phrase ul`
+    let ul = document.getElementById('phrase').firstElementChild;
     for (let i = 0; i < arr.length; i++) {
-      const li = document.createElement("li");
-      arr.firstElementChild = arr[i];
-      let array = "<li>" + arr[i] + "</li>";
-      return array;
+      let li = document.createElement("li");
+      li.textContent = arr[i];
+      isLetter(li);
+      ul.appendChild(li);
     }
   }
 
   addPhraseToDisplay(phraseArray);
-  console.log(phraseArray);
+
+  function checkLetter(buttonPressed, letterArray) {
+    if (buttonPressed.tagName === 'BUTTON') {
+      for (let i = 0; i < letterArray.length; i++) {
+        if (buttonPressed.textContent.toLowerCase() ===  letterArray[i].textContent.toLowerCase())
+        letterArray[i].className = 'show letter';
+      }
+    } else {
+      return null;
+    }
+  }
+
+  document.addEventListener('click', e => {
+    const classLetter = document.getElementsByClassName('letter');
+    const button = e.target;
+    checkLetter(button, classLetter);
+  });
+  
 });
+

a1bc0de3766cfd64edda5a22b1f25e203a6c71ad
Amanda Harness <16675876+meacoding@users.noreply.github.com>
2018-11-11 17:42:17 -0500

    Added minor features and refactored js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
+/*jshint esversion: 6 */
+
 document.addEventListener("DOMContentLoaded", e => {
-  const qwerty = document.getElementById("qwerty");
+  const overlay = document.getElementById("overlay");
   const phrase = document.getElementById("phrase");
+  const ul = phrase.firstElementChild;
   const btnReset = document.querySelector(".btn__reset");
+  // const qwerty = document.getElementById("qwerty");
+  const buttons = document.querySelectorAll("button");
+  const tries = document.getElementsByClassName("tries");
+  const p = document.createElement("p");
   let missed = 0;
   let letterFound;
   let usedLetters = [];
-  const overlay = document.getElementById("overlay");
-  const p = document.createElement("p");
-  overlay.insertBefore(p, btnReset);
+  let number;
 
   const phrases = [
     "Time is money friend",
@@ -17,140 +22,169 @@ document.addEventListener("DOMContentLoaded", e => {
     "My toes are pristine"
   ];
 
-  function getRandomPhraseAsArray(arr) {
-    let random = Math.floor(Math.random() * (arr.length - 0)) + 0;
-    let returnedArray = arr[random].split("", arr[random].length);
-    return returnedArray;
-  }
+  const actions = {
+    getRandomPhraseAsArray: arr => {
+      let random = Math.floor(Math.random() * (arr.length - 0)) + 0;
+      let returnedArray = arr[random].split("", arr[random].length);
+      return returnedArray;
+    },
 
-  function isLetter(char) {
-    let character = char.textContent;
-    if (character === " ") {
-      char.className = "space";
-    } else {
-      char.className = "letter";
-    }
-  }
+    isLetter: char => {
+      let character = char.textContent;
+      if (character === " ") {
+        char.className = "space";
+      } else {
+        char.className = "letter";
+      }
+    },
 
-  function addPhraseToDisplay(arr) {
-    let ul = document.getElementById("phrase").firstElementChild;
-    for (let i = 0; i < arr.length; i++) {
-      let li = document.createElement("li");
-      li.textContent = arr[i];
-      isLetter(li);
-      ul.appendChild(li);
-    }
-  }
-  function reset() {
-    const overlay = document.getElementById("overlay");
-    btnReset.textContent = "Reset";
-    overlay.style.display = "none";
+    addPhraseToDisplay: arr => {
+      for (let i = 0; i < arr.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = arr[i];
+        actions.isLetter(li);
+        ul.appendChild(li);
+      }
+    },
 
-    let list = document.getElementById("phrase").firstElementChild;
-    while (list.hasChildNodes()) {
-      list.removeChild(list.firstChild);
-    }
+    reset: () => {
+      usedLetters = [];
+      btnReset.textContent = "Reset";
+      overlay.style.display = "none";
 
-    const tries = document.getElementsByClassName("tries");
+      for (let i = missed - 1; i >= 0; i--) {
+        let img = tries[i].children[0];
+        img.style.display = "inline-block";
+        missed = 0;
+      }
 
-    for (let i = missed - 1; i >= 0; i--) {
-      let img = tries[i].children[0];
-      img.style.display = "inline-block";
-      missed = 0;
-    }
+      while (ul.hasChildNodes()) {
+        ul.removeChild(ul.firstChild);
+      }
 
-    const buttonQuery = document.querySelectorAll("button");
+      for (let i = 0; i < buttons.length; i++) {
+        buttons[i].className = "";
+        buttons[i].disabled = false;
+      }
+    },
 
-    for (let i = 0; i < buttonQuery.length; i++) {
-      buttonQuery[i].className = "";
-      buttonQuery[i].disabled = false;
-    }
+    checkLetter: buttonPressed => {
+      const classLetter = document.getElementsByClassName("letter");
+      let count = 0;
+      let i;
+      for (i = 0; i < classLetter.length; i++) {
+        if (buttonPressed === classLetter[i].textContent.toLowerCase()) {
+          classLetter[i].className += " show";
+          letterFound = buttonPressed;
+          count++;
+        }
+      }
+      if (count === 0) {
+        letterFound = null;
+      }
+    },
 
-    usedLetters = [];
-  }
+    missedGuess: btnPressed => {
+      let img = tries[missed].children[0];
+      if (letterFound === null) {
+        img.style.display = "none";
+        btnPressed.className += " wrong";
+        missed++;
+      }
+    },
 
-  function checkLetter(buttonPressed) {
-    const classLetter = document.getElementsByClassName("letter");
-    let count = 0;
-    for (let i = 0; i < classLetter.length; i++) {
-      if (buttonPressed === classLetter[i].textContent.toLowerCase()) {
-        classLetter[i].className = "show letter";
-        letterFound = buttonPressed;
-        count++;
+    checkWin: () => {
+      const show = document.getElementsByClassName("show");
+      const letter = document.getElementsByClassName("letter");
+      const youWinLose = status => {
+        overlay.className = `${status}`;
+        p.textContent = `You ${status}!`;
+        overlay.style.display = "flex";
+      };
+
+      if (missed < 5 && show.length === letter.length) {
+        usedLetters = [
+          65,
+          66,
+          67,
+          68,
+          69,
+          70,
+          71,
+          72,
+          73,
+          74,
+          75,
+          76,
+          77,
+          78,
+          79,
+          80,
+          81,
+          82,
+          83,
+          84,
+          85,
+          86,
+          87,
+          88,
+          89,
+          90
+        ];
+        const disableButtons = () => {
+          for (let i = 0; i < buttons.length; i++) {
+            buttons[i].disabled = true;
+            buttons[i].className = "chosen";
+          }
+        };
+
+        disableButtons();
+        setTimeout(() => {
+          youWinLose("win");
+        }, 5000);
+      } else if (missed >= 5) {
+        youWinLose("lose");
       }
     }
-    if (count === 0) {
-      letterFound = null;
-    }
-  }
+  };
 
-  function missedGuess() {
-    let tries = document.getElementsByClassName("tries");
-    let img = tries[missed].children[0];
-    if (letterFound === null) {
-      img.style.display = "none";
-      missed++;
-    }
-  }
-
-  function checkWin() {
-    const show = document.getElementsByClassName("show");
-    const letter = document.getElementsByClassName("letter");
-
-    if (missed < 5 && show.length === letter.length) {
-      overlay.style.display = "flex";
-      overlay.className = "win";
-      p.textContent = "You win!";
-    } else if (missed >= 5) {
-      overlay.style.display = "flex";
-      overlay.className = "lose";
-      p.textContent = "You lose!";
-    }
-  }
+  overlay.insertBefore(p, btnReset);
 
   btnReset.addEventListener("click", e => {
-    reset();
-    addPhraseToDisplay(getRandomPhraseAsArray(phrases));
+    actions.reset();
+    actions.addPhraseToDisplay(actions.getRandomPhraseAsArray(phrases));
   });
 
   document.addEventListener("click", e => {
+    const btn = e.target;
     let button = e.target;
-    button.disabled = true;
     if (button.tagName === "BUTTON") {
+      button.disabled = true;
       button.className = "chosen";
       button = button.textContent.toLowerCase();
-      console.log(button);
       usedLetters.push(button.toUpperCase().charCodeAt(0));
-
-      console.log(usedLetters);
-      checkLetter(button);
-      missedGuess();
-      checkWin();
+      actions.checkLetter(button);
+      actions.missedGuess(btn);
+      actions.checkWin();
     }
   });
 
   document.addEventListener("keyup", e => {
     let key = e.which;
+    const button = e.key.toLowerCase();
     if (key >= 65 && key <= 90 && !usedLetters.includes(key)) {
       usedLetters.push(key);
 
-      console.log(usedLetters);
-
-      const button = e.key.toLowerCase();
-      const buttonQuery = document.querySelectorAll("button");
-
-      for (let i = 0; i < buttonQuery.length; i++) {
-        if (button === buttonQuery[i].textContent) {
-          buttonQuery[i].className = "chosen";
-          buttonQuery[i].disabled = true;
+      for (let i = 0; i < buttons.length; i++) {
+        if (button === buttons[i].textContent) {
+          buttons[i].className = "chosen";
+          buttons[i].disabled = true;
+          number = i;
         }
       }
-
-      checkLetter(button);
-      missedGuess();
-      checkWin();
-    } else {
-      e.preventDefault();
+      actions.checkLetter(button);
+      actions.missedGuess(buttons[number]);
+      actions.checkWin();
     }
   });
 });
